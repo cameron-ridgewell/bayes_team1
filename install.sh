ee2865fc918be267c3ae2985d4441db1b7f6b994
cameron.ridgewell <cameron.ridgewell@gmail.com>
2016-03-31 10:21:39 -0400

    added gazebo2 and rviz to install.sh file

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+echo "Installing Gazebo2"
+sudo apt-get install gazebo2 -y
+
+echo "Installing rviz"
+sudo apt-get install ros-indigo-rviz -y
+
 echo "Installing Jackal Packages"
 sudo apt-get install ros-indigo-jackal-simulator ros-indigo-jackal-desktop -y
