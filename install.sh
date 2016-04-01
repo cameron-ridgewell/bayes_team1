9110eb6247f45fcc70a414e91fc9f91fe8c6a0ba
Cameron Ridgewell <cameron.ridgewell@gmail.com>
2016-04-01 18:02:57 -0400

    added openCV nonfree library as installation dependency

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ sudo apt-get install ros-indigo-rviz -y
 
 echo "Installing Jackal Packages"
 sudo apt-get install ros-indigo-jackal-simulator ros-indigo-jackal-desktop -y
+
+echo "Installing OpenCV NonFree"
+sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+sudo apt-get update
+sudo apt-get install libopencv-nonfree-dev -y
