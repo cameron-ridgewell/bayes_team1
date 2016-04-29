ba6a8dbbbd66de9c42251172b9ed9d8884796d45
Cameron Ridgewell <ridgewcp@vt.edu>
2016-04-29 19:08:11 -0400

    updated

--- catkin_ws/src/ip_webcam/src/motion_tracking_node.py
+++ catkin_ws/src/ip_webcam/src/motion_tracking_node.py
@@ -3,28 +3,7 @@ import rospy
 from std_msgs.msg import Float32MultiArray
 import urllib2
 import time 
-
-def control_motors(error_x, error_y):
-	if(error_x>15):
-		urlExecution(4)
-	else:
-		urlExecution(6)
-	if(error_y>15):
-		urlExecution(0)
-	else:
-		urlExecution(2)
-
-def urlExecution(command):
-	ip = 'http://192.168.1.41:81/decoder_control.cgi?loginuse=admin&loginpas=12345&command='
-	oneStep = '&onestep=1&'
-	gibberish = '7485621407675288&_='
-	timeStamp = int(time.time())*1000
-	fullURL = ip+str(command)+oneStep+str(timeStamp)+'.49641236611690986&_='+str(timeStamp)
-	response = urllib2.urlopen(fullURL)
-	time.sleep(0.2)
-	timeStamp = int(time.time())*1000
-	urllib2.urlopen(ip+"7"+'onestep=0&'+ str(timeStamp)+'.49641236611690986&_='+str(timeStamp))
-	rospy.loginfo(fullURL)
+from geometry_msgs.msg import Twist
 
 def callback(data):
 	rospy.loginfo(type(data.data))
@@ -39,11 +18,6 @@ def callback(data):
 
     
 def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('object_tracking', anonymous=True)
 
     rospy.Subscriber("/objects", Float32MultiArray, callback)
