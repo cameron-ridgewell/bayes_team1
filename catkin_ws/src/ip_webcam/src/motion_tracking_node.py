99317821591714eea2b769452db336afbe115b91
Cameron Ridgewell <ridgewcp@vt.edu>
2016-04-29 19:54:23 -0400

    not yet working, but updated

--- catkin_ws/src/ip_webcam/src/motion_tracking_node.py
+++ catkin_ws/src/ip_webcam/src/motion_tracking_node.py
@@ -4,17 +4,28 @@ from std_msgs.msg import Float32MultiArray
 import urllib2
 import time 
 from geometry_msgs.msg import Twist
+import cv2
+import numpy as np
 
 def callback(data):
-	rospy.loginfo(type(data.data))
 	rospy.loginfo(data.data)
 	if(len(data.data)>0):
-		rospy.loginfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-		rospy.loginfo('X: ' + str(data.data[9]))
-		rospy.loginfo('Y: ' + str(data.data[10]))
-		error_x= 320.0-data.data[9];
-		error_y= 240-data.data[10];
-		control_motors(error_x,error_y);
+		id = data.data[0];
+		objectWidth = data.data[1];
+		objectHeight = data.data[2];
+		cvHomography = cv2.CreateMat(3, 3, cv2.CV_32F)
+		cvHomography[0,0] = data.data[3];
+		cvHomography[1,0] = data.data[4];
+		cvHomography[2,0] = data.data[5];
+		cvHomography[0,1] = data.data[6];
+		cvHomography[1,1] = data.data[7];
+		cvHomography[2,1] = data.data[8];
+		cvHomography[0,2] = data.data[9];
+		cvHomography[1,2] = data.data[10];
+		cvHomography[2,2] = data.data[11];
+		inPts = [[0,0],[objectWidth,0],[0,objectHeight],[objectWidth,objectHeight]]
+		outPts = cv2.perspectiveTransform(inPts, cvHomography)
+		print outPts
 		#pub = rospy.Publisher("/ip_camera_motion", Twist, queue_size=1)
 
     
