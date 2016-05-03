90ff81ab0dcc41f0c3e117c46cedbe51e5725c31
Cameron Ridgewell <ridgewcp@vt.edu>
2016-05-03 11:23:46 -0400

    compiling

--- catkin_ws/src/ip_webcam/src/kalman_processor.cpp
+++ catkin_ws/src/ip_webcam/src/kalman_processor.cpp
@@ -60,17 +60,23 @@ class KalmanProcessor
 			&KalmanProcessor::update, this);
 		camera_angle_sub = nh_.subscribe("/ip_camera_angle", 10,
 			&KalmanProcessor::update_camera_angle, this);
+		image_pub_ = it_.advertise("/kalman_processor/image", 1);
 		detection_counter = DETECTION_MAX;
 		predicted_pos = Eigen::MatrixXf(2,1);
 		predicted_cov = Eigen::Matrix2f();
 		estimated_pos = Eigen::MatrixXf(2,1);
 		estimated_cov = Eigen::Matrix2f();
-		camera_pos = {0, 0};
+		camera_pos[0] = 0;
+		camera_pos[1] = 0;
 		initialized = false;
 	}
 
 	void predict(const sensor_msgs::ImageConstPtr& msg)
 	{
+		cv_bridge::CvImagePtr cv_ptr;// = cv_bridge::toCvShare(msg);
+		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		src = cv_ptr->image;
+
 		//only perform predictions if a detection has occured
 		if (initialized)
 		{
@@ -80,18 +86,30 @@ class KalmanProcessor
 
 			//Sensor Motion Model
 			Eigen::Matrix2f B = Eigen::Matrix2f();
-			B(0,0) = 
+			B(0,0) = src.cols / FOV_ANGLE;
+			B(1,1) = src.rows / FOV_ANGLE;
+
 			/*
 			 * TODO
 			 * Set u_k equal to the cumulative motion of the 
 			 * camera since the last update?
 			 */
 			//Sensor Motion
-			Eigen::MatrixXf(2,1) u_k = Eigen::MatrixXf(2,1);
+			Eigen::MatrixXf u_k = Eigen::MatrixXf(2,1);
+			u_k(0,0) = 0;	//camera yaw change since last predicition 
+			u_k(1,0) = 0;	//camera pitch change since last prection
+
+			//Process Noise Covariance
+			Eigen::Matrix2f Q = Eigen::Matrix2f();
+			Q(0,0) = CAMERA_MOTION_X_VAR;
+			Q(1,1) = CAMERA_MOTION_Y_VAR;
 
 			predicted_pos = F * estimated_pos + B * u_k;
 			predicted_cov = F * estimated_cov + Q;
+			drawCircle(cv::Point2f(predicted_pos(0,0), predicted_pos(1,0)), 3, 2);
 		}
+		sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", src).toImageMsg();
+		image_pub_.publish(img_msg);
 	}
 
 	void update(const std_msgs::Float32MultiArray::ConstPtr& msg)
@@ -116,13 +134,39 @@ class KalmanProcessor
 		//Sensor Model: H
 		Eigen::Matrix2f H = Eigen::Matrix2f();
 		H.setIdentity();
-		Eigen::MatrixXf resid = z_k - (H * predicted_pos);
 		
+		//Residual
+		Eigen::MatrixXf resid = z_k - (H * predicted_pos);
+
+		//Sensor Noise Covariance
+		Eigen::Matrix2f R = Eigen::Matrix2f();
+		R(0,0) = CAMERA_SENSOR_X_VAR;
+		R(1,1) = CAMERA_SENSOR_Y_VAR;
+
+		//Residual covariance
+		Eigen::Matrix2f resid_cov = H * predicted_cov * H.inverse() + R;
+
+		//Kalman Coefficient
+		Eigen::Matrix2f K = predicted_cov * H.inverse() * resid_cov.inverse();
+
+		//Estimated position
+		estimated_pos = predicted_pos + K * resid;
+
+		//Estimated covariance
+		estimated_cov = (Eigen::Matrix2f().setIdentity() - K * H) * predicted_cov;
+		
+		std::cout << estimated_pos << "\nend\n";
 	}
 
 	void update_camera_angle(const geometry_msgs::Twist& twist)
 	{
+		twist.angular.z; //yaw
+		twist.angular.x; //pitch
+	}
 
+	void drawCircle(const cv::Point2f point, const int radius, const size_t circle_width)
+	{
+		cv::circle(src, point, radius, cv::Scalar(255,0,0), circle_width);
 	}
 };
 
